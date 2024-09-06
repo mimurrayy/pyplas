a9937391dfc91b68ec176300663476b5c7980ec7
Julian Held <julian.held@gmail.com>
2024-09-06 14:46:06 -0500

    Bump version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyplas",
-    version="0.1.0",
+    version="0.2.0",
     author="Julian Held",
     author_email="julian.held@umn.edu",
     license='MIT',
